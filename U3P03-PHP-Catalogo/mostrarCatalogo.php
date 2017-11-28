1bd18a76bad183fe099a1cf41323209c31aa2a30
vilpab <vilpab@github.com>
2017-11-28 20:00:18 +0100

    Casa

--- U3P03-PHP-Catalogo/mostrarCatalogo.php
+++ U3P03-PHP-Catalogo/mostrarCatalogo.php
@@ -8,13 +8,10 @@ include "connection.php";
     <link href="./estilos/style.css" rel="stylesheet">
 </head>
 <body>
-<?php
-$ordenA=1;
-$orderD=0;
-?>
 <table>
-    </p><tr style='background-color:lightblue'>
-        <th>Id Obra <a href="mostrarCatalogo.php?order=<?php echo $ordenA?>"> &#9650</a></th>
+    <tr style='background-color:lightblue'>
+        <th>Id Obra <a href="mostrarCatalogo.php?order=1"> &#9650</a>
+            <a href="mostrarCatalogo.php?order=0"> &#9660</a></th>
         <th>Titulo </th>
         <th>Año</th>
         <th>Id Autor</th>
@@ -22,12 +19,30 @@ $orderD=0;
         <th>Imagen</th>
     </tr>
     <?php
-    if(isset($_REQUEST[$ordenA])){
-        $resultado = $conexion->query("SELECT * FROM musica,autor WHERE musica.idAutor=autor.idAutor ORDER BY musica.idObra DESC");
+    $order='';
+    $numero='';
+    if(isset($_GET["order"]) && $_GET["order"]==1){
+        $order='ASC';
+        $numero=1;
     }else{
-        $resultado = $conexion->query("SELECT * FROM musica,autor WHERE musica.idAutor=autor.idAutor");
+        $order='DESC';
+        $numero=0;
+    }
+    $autor='';
+    if(isset($_GET["autor"])){
+        $autor=$_GET["autor"];
+        $resultado = $conexion->query('SELECT * FROM musica,autor WHERE musica.idAutor=autor.idAutor AND autor.nombre="'.$autor.'" ORDER BY musica.idObra '. $order);
+
+    }else{
+        $resultado = $conexion->query('SELECT * FROM musica,autor WHERE musica.idAutor=autor.idAutor ORDER BY musica.idObra '. $order);
 
     }
+
+    // para limpiar querys seguridad ::
+    // $variable_limpia=  mysqli_real_escape_string($variable);
+
+
+
     if ($resultado->num_rows === 0) echo "<p>No hay animales en la base de datos</p>";
 
     while ($obra = $resultado->fetch_object('Obra')) {
@@ -35,7 +50,7 @@ $orderD=0;
         echo "<td>" . $obra->getIdObra() . "</td>\n";
         echo "<td>" . $obra->getTitulo() . "</td>\n";
         echo "<td>" . $obra->getAno() . "</td>\n";
-        echo "<td>" . $obra->getNombre() . "</td>\n";
+        echo "<td><a href='mostrarCatalogo.php?order=".$numero."&autor=".$obra->getNombre()."'>" . $obra->getNombre() . "</td>\n";
         if(!empty($obra->getDuracion())) echo "<td>" . $obra->getDuracion() . "</td>\n";else echo "<td> Sin datos </td>\n";
         echo "<td><a href='mostrarObra.php?idObra=".$obra->getIdObra()."'><img class='imagen' src='img/" . $obra->getImagen() . "'></a></td>\n";
 
